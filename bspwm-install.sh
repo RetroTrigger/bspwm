be8e72b599b0ef7e0fb7c7924016f15f2ab53bdc
RetroTrigger <66195617+RetroTrigger@users.noreply.github.com>
2022-04-02 11:38:55 +1000

    Update bspwm-install.sh

--- bspwm-install.sh
+++ bspwm-install.sh
@@ -6,6 +6,6 @@ cp -R IosevkaTermNerdFontComplete.ttf ~/.local/share/fonts
 cp -R .config/* ~/.config/
 chmod -R +x ~/.config/bspwm
 chmod -R +x ~/.config/polybar/scripts
-sudo pacman -Syu --needed --noconfirm - < packages-repository.txt
+cat packages-repository.txt | xargs sudo dnf -y install
 sudo fc-cache -f -v
 dbus-launch dconf load / < xed.dconf
